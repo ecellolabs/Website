036a041bc56f93aa82ff30382f682d7feb440a38
Momena <momena.akhtar19@gmail.com>
2025-10-31 21:57:44 +0500

    change logos/fontstyles etc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Ecello | Providing Cloud-based Solutions at Scale",
   description: "Providing Cloud-based Solutions at Scale",
@@ -28,7 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
