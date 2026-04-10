318a3efafc6c8cef35f2caaf22759f28afe34cb2
TalalMajeed <m.talal.majeed@gmail.com>
2026-04-11 01:49:00 +0500

    Fixed URL

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://api.ecello.net"
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.ecello.net"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
