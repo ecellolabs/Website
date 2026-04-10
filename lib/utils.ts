f975ad2db8a0fea0de1a01804ce403d86cca325b
TalalMajeed <m.talal.majeed@gmail.com>
2026-04-10 21:54:02 +0500

    Updated API

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5500"
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://api.ecello.net"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
